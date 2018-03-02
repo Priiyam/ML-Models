0ecb7de61e95116bc00ffa361683ab964788c0bb
priiyam <crifte@gmail.com>
2018-03-02 23:30:52 +0530

    K-Means Clustering added

--- 14. K-Means Clustering/priyam_kmeans.py
+++ 14. K-Means Clustering/priyam_kmeans.py
@@ -11,4 +11,8 @@ for i in range(1,11):
     kmeans = KMeans(n_clusters = i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
-
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
