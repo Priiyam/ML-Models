2ec04cfaa7168f4a40e90738b53b5565b660a7cf
priiyam <crifte@gmail.com>
2018-03-04 22:36:50 +0530

    Hierarchical Clustering added

--- 15. Hierarchical Clustering/priyam_hc.py
+++ 15. Hierarchical Clustering/priyam_hc.py
@@ -2,4 +2,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3,4]].values
+
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X, method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean Distances')
+plt.show()
 
