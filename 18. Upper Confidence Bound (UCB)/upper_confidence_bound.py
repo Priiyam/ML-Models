bcb07d382fb693dd5439c83b72693ff39cbfd4fb
priiyam <crifte@gmail.com>
2018-03-26 14:42:09 +0530

    UCB - Reinforcement Learning added

--- 18. Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 18. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -8,4 +8,12 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
+# Implementing UCB
+import math
+N = 10000
+d = 10
+ads_selected = []
+numbers_of_selections = [0] * d
+sums_of_rewards = [0] * d
+total_reward = 0
 
