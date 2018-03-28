3bd0d847e879de090e2874b97cd49844096980b8
priiyam <crifte@gmail.com>
2018-03-28 14:43:07 +0530

    UCB - Reinforcement Learning added

--- 18. Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ 18. Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -35,4 +35,9 @@ for n in range(0, N):
     sums_of_rewards[ad] = sums_of_rewards[ad] + reward
     total_reward = total_reward + reward
 
-
+# Visualising the results
+plt.hist(ads_selected)
+plt.title('Histogram of ads selections')
+plt.xlabel('Ads')
+plt.ylabel('Number of times each ad was selected')
+plt.show()
